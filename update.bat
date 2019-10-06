caec32323776acbd9d1764456352103b54f2b193
Paul Grayson <pdg@alum.mit.edu>
2019-10-06 12:27:05 -0700

    working update.bat

--- update.bat
+++ update.bat
@@ -1,4 +1,5 @@
-bash.exe -c 'gem install eventmachine --platform ruby'
 bash.exe -c 'gem install bundler'
+bash.exe -c 'gem uninstall eventmachine --force'
+bash.exe -c 'gem install eventmachine --platform ruby'
 bash.exe -c 'bundle update'
 @pause
